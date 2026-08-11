294e9dffea8f3be6e505c2bb71f476266edb3f62
ANIKETPROJECTS <aniketraneworkemail@gmail.com>
2026-08-11 21:33:07 +0530

    Change app name and update environment variables
    Updated application name and configuration settings.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "restaurant-pos",
+      name: "Bung-le-pos",
       script: "dist/index.js",
       instances: 1,
       autorestart: true,
@@ -9,9 +9,9 @@ module.exports = {
       max_memory_restart: "1G",
       env: {
         NODE_ENV: "production",
-        PORT: 8080, // Using 8080 as a safe alternative to 5000/3001
-        MONGODB_URI: process.env.MONGODB_URI,
-        SESSION_SECRET: process.env.SESSION_SECRET
+        PORT: 3019, // Using 8080 as a safe alternative to 5000/3001
+        MONGODB_URI: "mongodb+srv://raneaniket23_db_user:5MIiySrJlljOOOBY@bungle.hggnj83.mongodb.net/?appName=BUNGLE",
+        SESSION_SECRET: "aDauFfbM3ebs1JusMnBde31dZvn1lx6pT4kf4fSJm1o-RHHUOKZ1a0f0bciP1Dv4"
       }
     }
   ]
